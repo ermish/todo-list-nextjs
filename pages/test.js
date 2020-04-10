0a1aa7cf406431e6edc79aa4e0e24aa8b6b967a5
ermish <philipermish@gmail.com>
2020-04-10 22:42:37 +0900

    basic next example

---
+++ pages/test.js
@@ -0,0 +1,16 @@
+import Footer from '../components/footer'
+
+const Test = () => (
+  <div>
+    <main>
+      <h1 className="title">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <a href="/"> go home</a>
+      </h1>
+    </main>
+
+    <Footer></Footer>
+  </div>
+)
+
+export default Test
